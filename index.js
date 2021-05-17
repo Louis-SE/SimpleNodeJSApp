c11148cfccb8e8969ab117bb97840c16373126f6
Louis <louis.rivera.se@gmail.com>
2021-05-16 19:11:30 -0500

    Remove port and IP hard-coding
    The IP address and port were hard-coded into the app.
    Variables were created for them so that they can declared early on in the program,
    which makes finding their default values in code easier.
    This also sets up the application to accept command line argument for these values.

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-var http = require("http");
+var hostname = '127.0.0.1';
+var port = 8081;
+
+const console = require("console");
+const http = require("http");
 
 http.createServer(function(request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello World\n');
-}).listen(8081);
+}).listen(port, hostname);
 
-console.log('Server running at http://127.0.0.1:8081/');
+
+
+console.log(`Server running at http://${hostname}:${port}/`);
