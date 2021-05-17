c476e5a16581423773119694954e0093b2a42e5e
Louis <louis.rivera.se@gmail.com>
2021-05-16 19:42:06 -0500

    Add functions for setting server info from command line
    The initial version of the functions that will read the command line arguments were
    created. These will allow the user to enter a custom hostname and port for the
    application to use.

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
-var hostname = '127.0.0.1';
-var port = 8081;
-
+const { Console } = require("console");
 const console = require("console");
 const http = require("http");
+
+var myArgs = process.argv.slice(2);
+const defaultHostname = '127.0.0.1';
+const defaultPort = 8081;
+
+var hostname = setServerHostname(myArgs);
+var port = setServerPort(myArgs);
+
 
 http.createServer(function(request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
 }).listen(port, hostname);
 
-
-
 console.log(`Server running at http://${hostname}:${port}/`);
+
+
+// Retrieves a usable hostname from the command line args.
+// If a proper hostname was not entered in the command line, the default hostname is used.
+function setServerHostname(userArgs) {
+    if(userArgs.length >= 1) {
+        console.log("Possible Hostname Detected.");
+    }
+
+    return defaultHostname;
+}
+
+// Retrieves a usable port from the command line args.
+// If a proper port was not entered in the command line, the default port is used.
+function setServerPort(userArgs) {
+    if(userArgs.length >= 2 && typeof userArgs(2) == Number) {
+        console.log("Possible Port Detected.");
+    }
+    
+    return defaultPort;
+}
